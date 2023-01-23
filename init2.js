e79fbdd4a749a018f47d558293e32e5fe95e5b28
facu8990 <facu.vdp@gmail.com>
2023-01-22 23:40:55 -0300

    styling

--- init2.js
+++ init2.js
@@ -3,27 +3,27 @@ import inventory from './combine'
 const main = document.createElement('main');
 main.className += "container"
 
-document.getElementById('pcName').append(inventory.OS.CSName)
-const os = document.createElement('hgroup')
+document.getElementById('pcName').append(inventory.OS.CSName);
+const os = document.createElement('hgroup');
 os.innerHTML = `
     <h1>${inventory.OS.Caption}</h1>
     <small>${inventory.OS.Version}</small>
     <small>${inventory.OS.OSArchitecture}</small>
     <small>${inventory.OS.BuildNumber}</small>
     <small>${inventory.OS.SerialNumber}</small>
-`
-document.getElementById('os').appendChild(os)
-document.getElementById('os').appendChild(document.createElement('br'))
+`;
+document.getElementById('os').appendChild(os);
+document.getElementById('os').appendChild(document.createElement('br'));
 
 const singleComponent = (component) => {
     const content = {title: component[0], details: component[1]};
     const article = document.createElement('article');
     const header = document.createElement('header');
     const h1 = document.createElement('h1');
+    const details = document.createElement('ul');
     article.appendChild(header);
     header.appendChild(h1);
     h1.append(`${content.title}`);
-    const details = document.createElement('ul');
     Object.entries(content.details)
     .forEach(component => {
         if (component[1] !== null) {
@@ -37,33 +37,39 @@ const singleComponent = (component) => {
 };
 
 const appendMultiple = (component) => {
+
     const content = {title: component[0], details: component[1]};
     const article = document.createElement('article');
     const header = document.createElement('header');
     const hgroup = document.createElement('hgroup');
     const h1 = document.createElement('h1');
     const p = document.createElement('p');
-    article.appendChild(header);
-    header.appendChild(hgroup);
-    hgroup.appendChild(h1);
-    hgroup.appendChild(p);
-    h1.append(`${content.title}`);
-    p.append(`Devices found: ${content.details.length}`);
     const figure = document.createElement('figure');
     const table = document.createElement('table');
     const thead = document.createElement('thead');
     const trh = document.createElement('tr');
-    figure.appendChild(table)
-    table.appendChild(thead)
+    const tbody = document.createElement('tbody');
+
+    article.appendChild(header);
+    header.appendChild(hgroup);
+    hgroup.appendChild(h1);
+    hgroup.appendChild(p);
+    figure.appendChild(table);
+    table.appendChild(thead);
     thead.appendChild(trh);
+    article.appendChild(figure);    
+    table.appendChild(tbody);
+
+    h1.append(`${content.title}`);
+    p.append(`Devices found: ${content.details.length}`);
+
     Object.keys(content.details[0]).forEach(element => {
         const th = document.createElement('th');
         th.scope += 'col';
-        th.append(element)
+        th.append(element);
         trh.appendChild(th);
     });
-    const tbody = document.createElement('tbody');
-    table.appendChild(tbody)
+
     content.details.forEach(element => {
         const trb = document.createElement('tr');   
         Object.values(element).forEach(element => {
@@ -72,14 +78,13 @@ const appendMultiple = (component) => {
             trb.appendChild(td);
         });
         tbody.appendChild(trb);
-    })
-    article.appendChild(figure);    
+    });
+
     return article;
 };
 
 const appendSection = (section, input) => {
-    Object.entries(input)
-    .forEach(component => {
+    Object.entries(input).forEach(component => {
         if (component[1].length === undefined) {
             section.appendChild(singleComponent(component));
         } else {
@@ -88,7 +93,7 @@ const appendSection = (section, input) => {
     });
 };
 
-appendSection(main,inventory)
+appendSection(main,inventory);
 
 
-document.body.appendChild(main)
+document.body.appendChild(main);
